c44a766e086d8ce8eb06a034351275f82da84f77
SadafHaider <sadaf@findstoneage.com>
2023-12-05 12:25:22 +0500

    Fixes

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,214 +1,223 @@
 <template>
-    <!-- Big Logo Image Container -->
-    <div class="big-logo-container">
-        <img style="" src="/images/logo_main.png" alt="Big Logo Image" />
+<!-- Big Logo Image Container -->
+<div class="big-logo-container">
+    <img style="" src="/images/logo_main.png" alt="Big Logo Image" />
+</div>
+
+<!-- Text Box and Button -->
+    <div class="mainContainer">
+        <div class="centered-content">
+            <input type="text" class="rounded-input" placeholder="Drug Identification Number" />
+            <button class="rounded-button">Enter</button>
+        </div>
+
     </div>
 
-    <!-- Text Box and Button -->
-    <div class="centered-content">
-        <input type="text" class="rounded-input" placeholder="Drug Identification Number" />
-        <button class="rounded-button">Enter</button>
-    </div>
+<!-- Heading -->
+<div class="main-heading">How can Zimed® PF help?</div>
 
-    <!-- Heading -->
-    <div class="main-heading">How can Zimed® PF help?</div>
+<!-- Horizontal Line -->
+<div class="horizontal-line"></div>
+<div class="image-container"></div>
 
-    <!-- Horizontal Line -->
-    <div class="horizontal-line"></div>
-    <div class="image-container"></div>
-
-    <!-- Gradient Background with Image on the Right -->
-    <div class="centered-container">
-        <div class="gradient-background">
+<!-- Gradient Background with Image on the Right -->
+<div class="centered-container">
+    <div class="gradient-background">
+        <img src="/public/images/iStock1.png" alt="">
+    
+        <div class="box-effect">
             <div class="custom-box mb">
-                <div class="custom-text mt">
-                    ZIMED® PF's recommended dosage is one drop in the affected eye(s) once
-                    daily in the evening.
-                </div>
-            </div>
-
-            <div class="custom-box mb">
-                <div class="custom-text">
-                    ZIMED® PF is a 100% Preservative - free formula dispensed in a
-                    multi-dose bottle.
-                </div>
-            </div>
-
-            <div class="custom-box">
-                <div class="custom-text">
-                    ZIMED® PF is stored between 15 °C and 25 °C, no refrigeration needed.
-                </div>
+            <div class="custom-text">
+                ZIMED® PF's recommended dosage is one drop in the affected eye(s) once
+                daily in the evening.
             </div>
         </div>
-    </div>
 
-    <!-- Section 4 -->
-    <div class="main-heading">How to use Zimed® PF?</div>
-    <div class="horizontal-line"></div>
-
-    <div class="grid-container">
-        <!-- Row 1 -->
-        <div class="grid-item" style="background: white">
-            <div class="circle-container mb">
-                <img class="icon" src="/images/eye-dropper.png" />
+        <div class="custom-box mb">
+            <div class="custom-text">
+                ZIMED® PF is a 100% Preservative - free formula dispensed in a
+                multi-dose bottle.
             </div>
+        </div>
 
-            <div class="step-heading">Step 1</div>
-            <div class="step-description mt">
+        <div class="custom-box">
+            <div class="custom-text">
+                ZIMED® PF is stored between 15 °C and 25 °C, no refrigeration needed.
+            </div>
+        </div>
+        </div>
+        
+    </div>
+</div>
+
+<!-- Section 4 -->
+<div class="main-heading">How to use Zimed® PF?</div>
+<div class="horizontal-line"></div>
+
+<div class="grid-container">
+    <!-- Row 1 -->
+    <div class="grid-item">
+        <div class="circle-container mb">
+            <img class="icon" src="/images/eye-dropper.png" />
+        </div>
+
+        <div class="step-heading">Step 1</div>
+        <div class="step-description mt">
                 Before putting a drop in the eye, release the first drop away from the
-                eye to check the force needed to get only one drop out of the bottle.
-            </div>
-        </div>
-
-        <div class="grid-item">
-            <div class="circle-container mb">
-                <img class="icon" src="/images/washing-hands.png" />
-            </div>
-
-            <div class="step-heading">Step 2</div>
-            <div class="step-description mt">
-                Wash your hands. Tilt your head back and look at the ceiling.
-            </div>
-        </div>
-        <div class="grid-item">
-            <div class="circle-container mb">
-                <img class="icon" src="/images/eye.png" />
-            </div>
-
-            <div class="step-heading">Step 3</div>
-            <div class="step-description mt">
-                Gently pull down the lower eyelid until there is a small pocket.
-            </div>
-        </div>
-
-        <!-- Row 2 -->
-        <div class="grid-item">
-            <div class="circle-container mb">
-                <img class="icon" src="/images/icon-soft.png" />
-            </div>
-
-            <div class="step-heading">Step 4</div>
-            <div class="step-description mt">
-                Turn the bottle upside down and squeeze it to release one drop into each
-                eye that needs treatment.
-            </div>
-        </div>
-        <div class="grid-item">
-            <div class="circle-container mb">
-                <img class="icon" src="/images/eye-close.png" />
-            </div>
-
-            <div class="step-heading">Step 5</div>
-            <div class="step-description mt">
-                Let go of the lower lid and close your eye for 30 seconds.
-            </div>
-        </div>
-        <div class="grid-item">
-            <div class="circle-container mb">
-                <img class="icon" src="/images/toilet-paper.png" />
-            </div>
-
-            <div class="step-heading">Step 6</div>
-            <div class="step-description mt">
-                Wipe off any excess that runs down the cheek or other skin areas.
-            </div>
+            eye to check the force needed to get only one drop out of the bottle.
+            
         </div>
     </div>
 
-    <!-- Section 5 -->
-    <div class="white-background-section">
-        <!-- Image -->
-        <div class="left-cont">
-            <img src="/images/Fichier.png" alt="Example Image" />
+    <div class="grid-item">
+        <div class="circle-container mb">
+            <img class="icon" src="/images/washing-hands.png" />
         </div>
 
-        <!-- Text on the Right -->
-        <div class="text-container">
-            <div class="main-heading" style="text-align: left">Storing Zimed® PF</div>
+        <div class="step-heading">Step 2</div>
+        <div class="step-description mt">
+            Wash your hands. Tilt your head back and look at the ceiling.
+        </div>
+    </div>
+    <div class="grid-item">
+        <div class="circle-container mb">
+            <img class="icon" src="/images/eye.png" />
+        </div>
 
-            <div class="description-text mt">
-                Before putting the cap back on, shake the bottle once in a downwards
-                direction, without touching the dropper tip. This is to remove any
-                liquid on the tip. To avoid infections and eye injury, do not let the
-                tip of the bottle touch your eye or anything else. Put the cap back on
-                and close the bottle right after you have used it. Keep the bottle at
-                room temperature.
-            </div>
+        <div class="step-heading">Step 3</div>
+        <div class="step-description mt">
+            Gently pull down the lower eyelid until there is a small pocket.
         </div>
     </div>
 
-    <!-- Section 6 -->
-    <div class="split-container">
-        <div class="information-container">
-            <span class="information-text primary">
-                For more information, please see the
-            </span>
-            <span class="information-text accent">Patient Medication Information</span>
-            <span class="information-text primary">
-                or contact your Health Care professional.
-            </span>
+    <!-- Row 2 -->
+    <div class="grid-item">
+        <div class="circle-container mb">
+            <img class="icon" src="/images/icon-soft.png" />
         </div>
 
-        <div class="other-content">
-            <div class="second-heading">Dosing & Administration</div>
+        <div class="step-heading">Step 4</div>
+        <div class="step-description mt">
+            Turn the bottle upside down and squeeze it to release one drop into each
+            eye that needs treatment.
+        </div>
+    </div>
+    <div class="grid-item">
+        <div class="circle-container mb">
+            <img class="icon" src="/images/eye-close.png" />
+        </div>
 
-            <!-- box 1 -->
-            <div class="box mt">
-                <div class="split-box">
-                    <div class="left-col">
-                        <!-- Left column content, e.g., icon and text -->
-                        <div class="icon-container">
-                            <img src="/images/Vector.png" alt="Icon">
-                            <span class="text-icon">1x</span>
-                        </div>
-                    </div>
-                    <div class="right-col">
-                        <!-- Right column content, e.g., text -->
-                        <div class="text-container">
-                            The recommended dose is 1 drop in the affected eye(s) once daily in the evening.
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="step-heading">Step 5</div>
+        <div class="step-description mt">
+            Let go of the lower lid and close your eye for 30 seconds.
+        </div>
+    </div>
+    <div class="grid-item">
+        <div class="circle-container mb">
+            <img class="icon" src="/images/toilet-paper.png" />
+        </div>
 
-            <!-- box 2 -->
-            <div class="box">
+        <div class="step-heading">Step 6</div>
+        <div class="step-description mt">
+            Wipe off any excess that runs down the cheek or other skin areas.
+        </div>
+    </div>
+</div>
+
+<!-- Section 5 -->
+<div class="white-background-section">
+    <!-- Image -->
+    <div class="left-cont">
+        <img src="/images/Fichier.png" alt="Example Image" />
+    </div>
+
+    <!-- Text on the Right -->
+    <div class="text-container">
+        <div class="main-heading" style="text-align: left">Storing Zimed® PF</div>
+
+        <div class="description-text mt">
+            Before putting the cap back on, shake the bottle once in a downwards
+            direction, without touching the dropper tip. This is to remove any
+            liquid on the tip. To avoid infections and eye injury, do not let the
+            tip of the bottle touch your eye or anything else. Put the cap back on
+            and close the bottle right after you have used it. Keep the bottle at
+            room temperature.
+        </div>
+    </div>
+</div>
+
+<!-- Section 6 -->
+<div class="split-container">
+    <div class="information-container">
+        <span class="information-text primary">
+            For more information, please see the
+        </span>
+        <span class="information-text accent">Patient Medication Information</span>
+        <span class="information-text primary">
+            or contact your Health Care professional.
+        </span>
+    </div>
+
+    <div class="other-content">
+        <div class="second-heading">Dosing & Administration</div>
+
+        <!-- box 1 -->
+        <div class="box mt">
             <div class="split-box">
-                    <div class="left-col">
-                        <!-- Left column content, e.g., icon and text -->
-                        <div class="icon-container">
-                            <img src="/images/lense.png" alt="Icon">
-                            <span class="text-icon">1x</span>
-                        </div>
+                <div class="left-col">
+                    <!-- Left column content, e.g., icon and text -->
+                    <div class="icon-container">
+                        <img src="/images/Vector.png" alt="Icon">
+                        <span class="text-icon">1x</span>
                     </div>
-                    <div class="right-col">
-                        <!-- Right column content, e.g., text -->
-                        <div class="text-container">
-                        Do NOT use ZIMED® PF while you wear contact lenses.                    
-                        </div>
+                </div>
+                <div class="right-col">
+                    <!-- Right column content, e.g., text -->
+                    <div class="text-container">
+                        The recommended dose is 1 drop in the affected eye(s) once daily in the evening.
                     </div>
                 </div>
             </div>
+        </div>
 
-            <!-- box 3 -->
-            <div class="box">
+        <!-- box 2 -->
+        <div class="box">
             <div class="split-box">
-                    <div class="left-col">
-                        <!-- Left column content, e.g., icon and text -->
-                        <img style="padding: 0px 50px 0px 0px;" src="/images/Vector small.png" alt="Icon">
-
-                        <div class="icon-container">
-                            <img src="/images/Vector.png" alt="Icon">
-                        </div>
+                <div class="left-col">
+                    <!-- Left column content, e.g., icon and text -->
+                    <div class="icon-container">
+                        <img src="/images/lense.png" alt="Icon">
+                        <span class="text-icon">1x</span>
                     </div>
-                    <div class="right-col">
-                        <!-- Right column content, e.g., text -->
-                        <div class="text-container">
-                            The recommended dose is 1 drop in the affected eye(s) once daily in the evening.
-                        </div>
+                </div>
+                <div class="right-col">
+                    <!-- Right column content, e.g., text -->
+                    <div class="text-container">
+                        Do NOT use ZIMED® PF while you wear contact lenses.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- box 3 -->
+        <div class="box">
+            <div class="split-box">
+                <div class="left-col">
+                    <!-- Left column content, e.g., icon and text -->
+                    <img style="padding: 0px 50px 0px 0px;" src="/images/Vector small.png" alt="Icon">
+
+                    <div class="icon-container">
+                        <img src="/images/Vector.png" alt="Icon">
+                    </div>
+                </div>
+                <div class="right-col">
+                    <!-- Right column content, e.g., text -->
+                    <div class="text-container">
+                        The recommended dose is 1 drop in the affected eye(s) once daily in the evening.
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
